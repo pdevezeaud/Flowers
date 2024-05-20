6cb75e5a14aa17a9905f24898359528d890bb78b
Philippe <philippe@MACc02vg055hv2g.local>
2024-05-20 14:32:02 +0200

    deuxieme modification

--- main.js
+++ main.js
@@ -3,34 +3,42 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+ctx.globalCompositeOperation = 'Destination-over';
+let hue = Math.random() * 360;
 
 //Variable
 //let size = 0;
-let positionX = 0;
-let positionY = 0;
-let angle = 0;
+let number = 0;
+let scale = 10;
 
 
 function drawFlower(){
     //Circle
-    ctx.fillStyle = 'red';
-    ctx.strokeStyle = 'green';
-    ctx.lineWidth = 5;
+    let angle = number * 9.6;
+    let radius = scale * Math.sqrt(number);
+    let positionX = radius * Math.sin(angle) + canvas.width / 2;
+    let positionY = radius * Math.cos(angle) + canvas.height / 2;
+
+    ctx.fillStyle = 'hsl('+ hue +',100%,50%)';
+    ctx.strokeStyle = 'black';
+    ctx.lineWidth = 4;
     ctx.beginPath();
-    ctx.arc(positionX, positionY, 20,0,Math.PI * 2);
+    ctx.arc(positionX, positionY, 8,0,Math.PI * 2);
     ctx.closePath();
     ctx.fill()
     ctx.stroke();
 
+    number ++
+    hue += 0.5
+
 }
 
 function animate() {
-ctx.clearRect(0, 0, canvas.width, canvas.height);
-positionX += 0.5;
-positionY += 0.5;
-angle += 0.1;
+//ctx.clearRect(0, 0, canvas.width, canvas.height);
+
     
 drawFlower();
+if(number > 200) return
 
 
 requestAnimationFrame(animate);
